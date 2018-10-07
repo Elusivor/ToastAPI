947cf1ffee422e0dab6b9a807c83eb27ee923b9c
dev <dev@PSG-Brendan.orion.internal>
2018-10-08 10:40:12 +1300

    Initial commit

---
+++ src/main/java/org/toastmates/toastapi/ToastapiApplication.java
@@ -0,0 +1,12 @@
+package org.toastmates.toastapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToastapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToastapiApplication.class, args);
+	}
+}
